aab9af4b748be5aed02be6157e21ad51dd7d13b8
tyVespA <migliavacca.marco08@gmail.com>
2023-01-27 19:26:03 +0100

    Removed dependencies, modules and files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 // https://webpack.js.org/guides/getting-started/
 
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
 
 function component() {
   const element = document.createElement("div");
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  // Add the image to our existing div
-  const myIcon = new Image();
-  myIcon.scr = Icon;
-
-  element.appendChild(myIcon);
 
   return element;
 }
